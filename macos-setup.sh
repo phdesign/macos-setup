1ae792926cd87dca8f7674ab15bf77a76163d97f
Paul Heasley <paul.heasley@acloud.guru>
2020-08-18 10:56:01 +1000

    Revert to should_install

--- macos-setup.sh
+++ macos-setup.sh
@@ -2,6 +2,23 @@
 
 set -eu
 
+DRYRUN=0
+PARAMS=""
+while (( "$#" )); do
+    case "$1" in
+        -n)
+            DRYRUN=1
+            echo "Dry run is on, I won't actually install anything"
+            shift
+            ;;
+        *) # preserve remaining arguments
+            PARAMS="$PARAMS $1"
+            shift
+            ;;
+    esac
+done
+eval set -- "$PARAMS"
+
 olddir=$(pwd)
 basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$basedir"
--- scripts/brew.sh
+++ scripts/brew.sh
@@ -6,37 +6,30 @@ function has_command {
   return $(command -v "$1" > /dev/null)
 }
 
-function install_brew {
-  local dependency="brew"
-  if has_command $dependency; then
-    echo "⏩  $dependency is already installed"
-  else
-    echo "✅  Installing $dependency..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  fi
-}
-
 function has_formulae {
   return $(brew ls --versions "$1" > /dev/null)
 }
 
+function has_application {
+  return $(test -e "/Applications/$1.app")
+}
+
+function install_brew {
+  if should_install brew "has_command brew"; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  fi
+}
+
 function install_formulae {
   local dependency="$1"
-  if has_formulae $dependency; then
-    echo "⏩  $dependency is already installed"
-  else
-    echo "✅  Installing $dependency..."
+  if should_install $dependency "has_formulae $dependency"; then
     brew install "$dependency"
   fi
 }
 
 function install_iterm {
-  local dependency="iterm"
-  if has_application "iTerm"; then
-    echo "⏩  $dependency is already installed"
-  else
-    echo "✅  Installing $dependency..."
-    brew cask install "$dependency"
+  if should_install iterm "has_application 'iTerm'"; then
+    brew cask install iterm
     echo "📝  TODO: Manually configure iTerm2"
     echo "    - iTerm2 > Preferences > Profiles > Other Actions > Import JSON Profiles > 'config/Tomorrow-Night-Eighties.json'"
     echo "    - iTerm2 > Preferences > Appearance > General > Theme: Minimal"
@@ -45,17 +38,9 @@ function install_iterm {
   fi
 }
 
-function has_application {
-  return $(test -e "/Applications/$1.app")
-}
-
 function install_beyond_compare {
-  local dependency="beyond-compare"
-  if has_application "Beyond Compare"; then
-    echo "⏩  $dependency is already installed"
-  else
-    echo "✅  Installing $dependency..."
-    brew cask install "$dependency"
+  if should_install beyond-compare "has_application 'Beyond Compare'"; then
+    brew cask install beyond-compare
     echo "📝  TODO: Enter Beyond Compare license"
     if [ ! has_command bcompare ]; then
       ln -s /Applications/Beyond\ Compare.app/Contents/MacOS/bcomp /usr/local/bin/bcompare
@@ -65,17 +50,39 @@ function install_beyond_compare {
 }
 
 function install_node {
-  local dependency="nvm"
-  if has_command $dependency; then
-    echo "⏩  $dependency is already installed"
-  else
-    echo "✅  Installing $dependency..."
+  if should_install nvm "has_command nvm"; then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
     # Start nvm in current shell
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
     nvm install node
+  fi
+}
+
+function install_java {
+  if should_install java "has_command java"; then
+    brew cask install java
+  fi
+}
+
+function install_ctags {
+  if should_install universal-ctags "has_formulae universal-ctags"; then
+    brew install --HEAD universal-ctags/universal-ctags/universal-ctags
+  fi
+}
+
+function install_typora {
+  if should_install typora "has_application 'Typora'"; then
+    brew cask install typora
+    curl -b cookies.txt -L \
+      "https://github.com/elitistsnob/typora-gitlab-theme/releases/download/v1.1/typora-gitlab-theme-master-updated.zip" \
+      -o typora-gitlab-theme-master-updated.zip
+    unzip typora-gitlab-theme-master-updated -d typora-gitlab-theme-master-updated
+    mv typora-gitlab-theme-master-updated/gitlab* "/Users/paul.heasley/Library/Application Support/abnerworks.Typora/themes"
+    echo "📝  TODO: Manually configure Typora"
+    echo "    - Typora > Preferences > General > On launch: Open custom folder"
+    echo "    - Typora > Preferences > Image > When insert...: Copy image to ./\${filename}.assets"
   fi
 }
 
@@ -87,12 +94,11 @@ install_formulae coreutils
 # Install Python
 install_formulae python
 
-# Custom tools
+# Command line tools
 install_formulae jq
 install_formulae the_silver_searcher
 install_formulae tree
 install_formulae z
-install_formulae neovim
 
 # iTerm2
 install_iterm
@@ -104,24 +110,14 @@ install_java
 install_beyond_compare
 
 # Typora
-function install_typora {
-  install_cask typora
-  curl -b cookies.txt -L \
-    "https://github.com/elitistsnob/typora-gitlab-theme/releases/download/v1.1/typora-gitlab-theme-master-updated.zip" \
-    -o typora-gitlab-theme-master-updated.zip
-  unzip typora-gitlab-theme-master-updated -d typora-gitlab-theme-master-updated
-  mv typora-gitlab-theme-master-updated/gitlab* "/Users/paul.heasley/Library/Application Support/abnerworks.Typora/themes"
-  echo "📝  TODO: Manually configure Typora"
-  echo "    - Typora > Preferences > General > On launch: Open custom folder"
-  echo "    - Typora > Preferences > Image > When insert...: Copy image to ./\${filename}.assets"
-}
-partial needs_typora needs_application "Typora"
-install typora needs_typora install_typora
+install_typora
 
 # Node
-install_node
+#install_node
 
 # Vim
+install_formulae neovim
+install_ctags
 #install_esctags
 
 # Todo
