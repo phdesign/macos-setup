255ad0687fa3e909c63bb506e3294166dcdc1951
Paul Heasley <paul.heasley@acloud.guru>
2020-12-21 09:27:57 +1100

    Adding MacVim setup

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -2,52 +2,37 @@
 
 set -eu
 
-function install_ctags {
-    if should_install universal-ctags "has_formulae universal-ctags"; then
-        brew install --HEAD universal-ctags/universal-ctags/universal-ctags
-    fi
-}
+vim_dir="$HOME/.vim"
 
 function configure_vim {
-    if should_configure vim "has_folder \"$HOME/.config/nvim\""; then
-        ln -s $(pwd)/config/nvim ~/.config
-
-        # Install pynvim
-        python3 -m pip install --user --upgrade pynvim
-
-        cat << EOM
-📝  TODO: Setup NeoVim macOS shortcut
-    - Create applescript application using './config/neovim.applescript'
-    - Copy icon from https://github.com/neovim/neovim.github.io/blob/master/logos/neovim-mark.png
-    - Right click on application, click 'Get Info', select icon and paste
-    - Drag application to Dock
-EOM
-    fi
-}
-
-function configure_vim_docs {
-    local vim_docs_dir="${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/doc"
-    if should_configure vim-docs "has_folder \"$vim_docs_dir\""; then
-        mkdir -p $vim_docs_dir
-        ln -s $(pwd)/config/vim-cheatsheet.txt $vim_docs_dir
-        nvim -es -u ~/.config/nvim/init.vim -i NONE -c "helptags $vim_docs_dir" -c "qa" || true
+    if should_configure vim "has_folder \"$vim_dir\""; then
+        mkdir $vim_dir 
+        # Temp folder is required for swap files
+        mkdir "$vim_dir/tmp"
+        ln -s $(pwd)/config/vimrc $vim_dir
     fi
 }
 
 function install_vim_plug {
-    # Must be run after configure_vim so that the init.vim exists
-    local dest="${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim"
+    local dest="$vim_dir/autoload/plug.vim"
     if should_install vim-plug "has_file \"$dest\""; then
         sh -c "curl -fLo \"$dest\" --create-dirs \
            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
         echo "✅  Installing vim plugins..."
-        nvim -es -u ~/.config/nvim/init.vim -i NONE -c "PlugInstall" -c "qa" || true
+        vim -es -u vimrc -i NONE -c "PlugInstall" -c "qa" || true
     fi
 }
 
-install_formulae neovim
-install_ctags
-#install_esctags
+function configure_vim_docs {
+    local vim_docs_dir="$vim_dir/doc"
+    if should_configure vim-docs "has_folder \"$vim_docs_dir\""; then
+        mkdir -p $vim_docs_dir
+        ln -s $(pwd)/config/vim-cheatsheet.txt $vim_docs_dir
+        vim -es -u vimrc -i NONE -c "helptags $vim_docs_dir" -c "qa" || true
+    fi
+}
+
+install_formulae macvim
 configure_vim
 install_vim_plug
 configure_vim_docs
